bb7c8cab9f6c1a7da6b12771b3bfc78b728c4d91
alcantarajulio <julioalc21@gmail.com>
2025-02-16 14:35:31 -0300

    feat: add HtmlFetcher

---
+++ HtmlFetcher.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class HtmlFetcher {
+    public List<String> fetchHtmlLines(String url) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
