3072aa498c72ecf2a9089a4f2306ddf0d47deeec
alcantarajulio <julioalc21@gmail.com>
2025-02-16 14:36:48 -0300

    update htmlanalyzer

--- HtmlAnalyzer.java
+++ HtmlAnalyzer.java
@@ -1,7 +1,17 @@
+import java.util.List;
+
 public class HtmlAnalyzer {
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage: java HtmlAnalyzer <url>");
         }
+        HtmlFetcher fetcher = new HtmlFetcher();
+        List<String> htmlLines = fetcher.fetchHtmlLines(args[0]);
+        if (htmlLines == null) {
+            System.out.println("Url connection error");
+        } else {
+            System.out.println("Url connection success");
+        }
+
     }
 }
